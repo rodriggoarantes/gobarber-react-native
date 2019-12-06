a5a379416a16bdb8791121e0fa56b2751c7e5c6c
rodrigo <rodrigo.arantes@totvs.com.br>
2019-12-06 13:42:24 -0300

    Criaçao dos componentes Input e Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Background from '~/components/Background';
+import Button from '~/components/Button';
 import Input from '~/components/Input';
 
 import { Container } from './styles';
@@ -9,7 +10,13 @@ export default function SignIn() {
   return (
     <Background>
       <Container>
-        <Input icon="call" placeholder="Digite o seu nome" />
+        <Input
+          style={{ marginTop: 30 }}
+          icon="call"
+          placeholder="Digite o seu nome"
+        />
+
+        <Button>Entrar</Button>
       </Container>
     </Background>
   );
